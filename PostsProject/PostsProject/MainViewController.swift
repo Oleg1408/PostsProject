23582e8c7db1d3b8b0bc99530b934eb9aed6d418
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-08-31 21:48:49 +0300

    Screen Orientation

---
+++ PostsProject/PostsProject/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PostsProject
+//
+//  Created by Олег Курбатов on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ PostsProject/PostsProject/SettingsPostsController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsPostsController.swift
+//  PostsProject
+//
+//  Created by Олег Курбатов on 31.08.2023.
+//
+
+import UIKit
+
+class SettingsPostsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+
+}
